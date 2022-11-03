df482ce80aa69574eabc7ddc6a2b384b637b9f33
Fedor Sharov <1710193@mail.ru>
2022-11-03 07:08:26 +0300

    added new branche - 'junit4-platform' and added tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
 
@@ -83,4 +85,85 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+    //_______________________________________________________________________
+//покупка в пределах лимита
+    @Test
+    public void showHowMuchToSpendIfThePurchaseIsLessThanTheLimit_API() {
+
+        int actual = cashbackHackService.remain(500);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    //покупка больше лимита
+    @Test
+    public void showHowMuchToSpendIfThePurchaseIsOverThanTheLimit_API() {
+
+        int actual = cashbackHackService.remain(1500);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    //покупка равна лимиту
+    @Test
+    public void showHowMuchToSpendIfThePurchaseIsEqualToTheLimit_API() {
+
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    //верхнее граничные значения - меньше
+    @Test
+    public void upperLimitValueLessThanLimit_API() {
+
+        int actual = cashbackHackService.remain(999);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    //верхнее граничные значения - больше
+    @Test
+    public void upperLimitValueGreaterThanLimit_API() {
+
+        int actual = cashbackHackService.remain(1001);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    //покупка ноль
+    @Test
+    public void ifPurchaseEqualNull_API() {
+
+        int actual = cashbackHackService.remain(0);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    //покупка отрицательная
+    @Test
+    public void ifPurchaseNegative_API() {
+
+        int actual = cashbackHackService.remain(-1);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    //нижнее граничное значение
+    @Test
+    public void bottonLimitValueGreaterThanLimit_API() {
+
+        int actual = cashbackHackService.remain(1);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
 }
